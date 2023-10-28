73f69eaa8a41845090a6719a9f9dc27188e12504
antegral <antegral@antegral.net>
2023-10-28 18:36:30 +0900

    .

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -16,8 +16,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { connect } from 'vectordb';
+import { LanceDB } from 'langchain/vectorstores/lancedb';
 
-@WebSocketGateway({
+@WebSocketGateway(3630, {
   cors: {
     origin: '*',
   },
@@ -52,6 +54,17 @@ export class ChatGateway {
 
   @SubscribeMessage('chat')
   async onEvent(client: any, data: any) {
+    const dir = 'data/vector-database';
+
+    const db = await connect(dir);
+    const table = await db.openTable('vectors');
+    const vectorStore = new LanceDB(
+      new OpenAIEmbeddings({
+        openAIApiKey: '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',
+      }),
+      { table },
+    );
+
     const req = JSON.parse(data);
     const session_id = uuidv4();
     let resp_token = 0;
@@ -63,26 +76,66 @@ export class ChatGateway {
           role: 'system',
           content: `나는 부동산 전세사기에 대해 고객에게 설명해주는 부동산 전문가다. 나는 다음과 같은 지식을 알고 있다.\n${this.system_prompt}\n나는 따라서, 고객에게 전세사기에 관해 설명할때 전문용어를 사용하여 정보를 알려줘야 한다. 피해 상황이라면 무조건 경찰에 신고하라는 말 대신, 상황에 맞는 실제 임대차 사기 관련 상담 연락처를 남겨주어야 한다.`,
         },
-        {
-          role: 'user',
-          content:
-            '해커톤 오피스텔 세입자인데, 전세사기가 의심돼요. 전세금 1억 6,500만원 / 전세 기간 2022.01.08 ~ 2023.12.07 인데요 계약 시 부동산에서 개인주인집을 만나 계약을 했어요. 당시 근저당 잡혀있어 부동산 조약에 근저당 해소 특약 걸었고,잔금 입금 후 임대인이 채권 해제 후에 입주하고 전세권 설정도 하고 전입신고와 확정일자를 받았어요. 보험은 가입하지 않았어요. 전세 사기가 유행이라 혹시 몰라 등기를 떼어 보니 집주인이 2022년 2월에 어떤 법인회사로 변경되어 있었어요. 한 달 후에 바로 팔았어요. 아직 등기상 근저당은 없으나 법인이 소유주인데 전화번호도 없는 번호라고 떠요. 아직 만기까지 7개월 남았는데 제가 할 수 있는게 아무것도 없는 거 같아요. 어떻게 해야 할까요?',
-        },
-        {
-          role: 'assistant',
-          content:
-            '전세사기 피해를 회복하기 위해 (1) 형사고소, (2) 보증금반환청구소송, (3) 채권추심/강제집행이 있습니다. 공인중개사가 전세사기에 적극 가담했다면 (4) 공인중개사 및 중개사협회에 대한 손해배상청구도 생각해볼 수 있습니다.\n이를 위해, 대한법률구조공단의 상담을 받을 수 있으며, 132로 전화하면 됩니다.\n경기도민이라면, 경기도 무료법률상담실 (031-120, 031-8008-2246)에 연락하여 도움을 구할 수 있습니다.',
-        },
-        {
-          role: 'user',
-          content:
-            '경기도에서 전세사기 관련해서 법률 상담을 할 수 있는 연락처를 알려주세요.',
-        },
-        {
-          role: 'assistant',
-          content:
-            '경기도 무료법률상담실 (031-120, 031-8008-2246)에 연락하여 도움을 구할 수 있습니다. 또한, 경기도 열린민원실(031-120)에 연락할 수도 있습니다.',
-        },
+        // {
+        //   role: 'user',
+        //   content: '깡통전세(역전세)이 왜 문제인지 알려주세요.',
+        // },
+        // {
+        //   role: 'assistant',
+        //   content: (
+        //     await vectorStore.similaritySearch('깡통주택이 문제인 이유', 1)
+        //   )[0].pageContent,
+        // },
+        // {
+        //   role: 'user',
+        //   content:
+        //     '경기도에서 전세사기 관련해서 법률 상담을 할 수 있는 연락처를 알려주세요.',
+        // },
+        // {
+        //   role: 'assistant',
+        //   content: (
+        //     await vectorStore.similaritySearch(
+        //       '경기도에서 전세사기 관련해서 법률 상담을 할 수 있는 연락처',
+        //       1,
+        //     )
+        //   )[0].pageContent,
+        // },
+        // {
+        //   role: 'user',
+        //   content: '전세사기를 예방하기 위해서 어떻게 해야하는지 설명해주세요.',
+        // },
+        // {
+        //   role: 'assistant',
+        //   content: (
+        //     await vectorStore.similaritySearch(
+        //       '깡통주택을 예방하기 위해 할 수 있는 일',
+        //       1,
+        //     )
+        //   )[0].pageContent,
+        // },
+        // {
+        //   role: 'user',
+        //   content: '보증금을 돌려주지 않는다면 어떻게 해야하나요?',
+        // },
+        // {
+        //   role: 'assistant',
+        //   content: (
+        //     await vectorStore.similaritySearch(
+        //       '보증금을 돌려주지 않을 때 사용할 수 있는 방법',
+        //       1,
+        //     )
+        //   )[0].pageContent,
+        // },
+        // {
+        //   role: 'user',
+        //   content:
+        //     '해커톤 오피스텔 세입자인데, 전세사기가 의심돼요. 전세금 1억 6,500만원 / 전세 기간 2022.01.08 ~ 2023.12.07 인데요 계약 시 부동산에서 개인주인집을 만나 계약을 했어요. 당시 근저당 잡혀있어 부동산 조약에 근저당 해소 특약 걸었고,잔금 입금 후 임대인이 채권 해제 후에 입주하고 전세권 설정도 하고 전입신고와 확정일자를 받았어요. 보험은 가입하지 않았어요. 전세 사기가 유행이라 혹시 몰라 등기를 떼어 보니 집주인이 2022년 2월에 어떤 법인회사로 변경되어 있었어요. 한 달 후에 바로 팔았어요. 아직 등기상 근저당은 없으나 법인이 소유주인데 전화번호도 없는 번호라고 떠요. 아직 만기까지 7개월 남았는데 제가 할 수 있는게 아무것도 없는 거 같아요. 어떻게 해야 할까요?',
+        // },
+        // {
+        //   role: 'assistant',
+        //   content:
+        //     '전세사기 피해를 회복하기 위해 (1) 형사고소, (2) 보증금반환청구소송, (3) 채권추심/강제집행이 있습니다. 공인중개사가 전세사기에 적극 가담했다면 (4) 공인중개사 및 중개사협회에 대한 손해배상청구도 생각해볼 수 있습니다.\n이를 위해, 대한법률구조공단의 상담을 받을 수 있으며, 132로 전화하면 됩니다.\n경기도민이라면, 경기도 무료법률상담실 (031-120, 031-8008-2246)에 연락하여 도움을 구할 수 있습니다.',
+        // },
         {
           role: 'user',
           content: req.message,
--- src/main.ts
+++ src/main.ts
@@ -4,32 +4,35 @@ import { AppModule } from './app.module';
 import { LanceDB } from 'langchain/vectorstores/lancedb';
 import { connect } from 'vectordb';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import OpenAI from 'openai';
+import { CharacterTextSplitter } from 'langchain/text_splitter';
+import { readFile, rm } from 'fs/promises';
 
 async function genDatabase() {
-  const loader = new TextLoader('data/expression.txt');
-  const docs = await loader.load();
-
+  await rm('data/vector-database', { recursive: true });
   const dir = 'data/vector-database';
+
+  const text = await readFile('data/unvectorized_database.txt', 'utf8');
+
+  const splitter = new CharacterTextSplitter({
+    separator: '\n\n',
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
 
   const db = await connect(dir);
   const table = await db.createTable('vectors', [
-    { vector: Array(1536), text: 'Hello', source: 'a' },
+    { vector: Array(1536), text: 'sample', loc: { lines: { from: 0, to: 0 } } },
   ]);
 
+  const output = await splitter.createDocuments([text]);
+
   const vectorStore = await LanceDB.fromDocuments(
-    docs,
+    output,
     new OpenAIEmbeddings({
       openAIApiKey: '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',
     }),
     { table },
   );
-
-  console.dir(vectorStore);
-
-  console.log('TEST: Search 전세사기');
-  const testResult = await vectorStore.similaritySearch('전세사기', 1);
-  console.log(testResult);
 }
 
 async function testDatabase() {
@@ -45,23 +48,37 @@ async function testDatabase() {
     { table },
   );
 
-  console.log('TEST: Search 깡통전세란?');
-  let testResult = await vectorStore.similaritySearch('확정일자란?', 1);
+  console.log('TEST: Search Q1');
+  let testResult = await vectorStore.similaritySearch(
+    '깡통주택이 문제인 이유',
+    1,
+  );
   console.log(testResult);
 
-  console.log('TEST: Search 전세사기란?');
-  testResult = await vectorStore.similaritySearch('전세사기란?', 1);
+  console.log('TEST: Search Q2');
+  testResult = await vectorStore.similaritySearch(
+    '경기도에서 전세사기 관련해서 법률 상담을 할 수 있는 연락처',
+    1,
+  );
   console.log(testResult);
 
-  console.log('TEST: Search 선순위 대출이란?');
-  testResult = await vectorStore.similaritySearch('선순위 대출이란?', 1);
+  console.log('TEST: Search Q3');
+  testResult = await vectorStore.similaritySearch(
+    '깡통주택을 예방하기 위해 할 수 있는 일',
+    1,
+  );
+  console.log(testResult);
+
+  console.log('TEST: Search Q4');
+  testResult = await vectorStore.similaritySearch(
+    '보증금을 돌려주지 않을 때 사용할 수 있는 방법',
+    1,
+  );
   console.log(testResult);
 }
 
 async function bootstrap() {
-  await genDatabase();
-
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(7000);
 }
 bootstrap();
